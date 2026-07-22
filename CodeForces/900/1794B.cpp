d933216a8498cef66dbc79a31d3854794410697c
Kushagra Patel <patelkushagra827@gmail.com>
2026-07-23 00:13:54 +0530

    Solved 1794B CodeForces

---
+++ CodeForces/900/1794B.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>nums(n);
+        for(int i = 0; i < n; i++){
+            cin>>nums[i];
+        }
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 1){
+                nums[i]++;
+            }
+        }
+
+        for(int i = 0; i < n-1; i++){
+            int a = nums[i];
+            int b = nums[i+1];
+            
+            if(b % a == 0){
+                nums[i+1]++;
+            }
+        }
+        for(int i = 0;i < n; i++){
+            cout<<nums[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
