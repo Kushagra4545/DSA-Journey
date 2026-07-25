4d0ea7f737c624f1ffa2acd30c583987dc8d2f78
Kushagra Patel <patelkushagra827@gmail.com>
2026-07-25 21:05:36 +0530

    Updated BNY questions

---
+++ Online Assessment/BNY/Q4.cpp
@@ -0,0 +1,16 @@
+/*
+Give numbers of arrays with
+given n : number of elements in array 
+given m : range from which [1, m];
+
+cost = this is how many times max element is 
+        getting updated 
+
+Count number of arrays you can generate.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int helper(){
+
+}
