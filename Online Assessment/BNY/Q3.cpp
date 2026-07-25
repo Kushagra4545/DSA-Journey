c01581d6f423c5ef227a8656d67ce49982ba6ac5
Kushagra Patel <patelkushagra827@gmail.com>
2026-07-25 20:59:12 +0530

    Solved BNY questions

---
+++ Online Assessment/BNY/Q3.cpp
@@ -0,0 +1,16 @@
+/*
+Make an Array Palindrome with minimum no of operation. 
+Operation: you can replace any two adjacent no. with 
+their sum
+
+Leetcode - 2422
+Problem Link - https://leetcode.com/problems/merge-operations-to-turn-array-into-a-palindrome/
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int helper(){
+    
+}
