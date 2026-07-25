94d4498faa630c59496f9eef8f9927285c5dffc6
Kushagra Patel <patelkushagra827@gmail.com>
2026-07-25 18:50:15 +0530

    Added question of BNY

---
+++ Online Assessment/BNY/Q1.cpp
@@ -0,0 +1,22 @@
+/*
+A city of hackerLand has an even number of house n
+arranged in row. A painter needs to paint these houses 
+conditions:
+1- Adjacent should not be of same color
+2- houses equidistant from both end must not be of same color
+eg. if n = 6 then 1 and 6 can not have same color 
+
+find the total Number of combinations of coloring all the houses
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int totalCombinations(int n){
+    
+}
+
+int main(){
+
+}
