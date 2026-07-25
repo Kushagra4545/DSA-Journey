30fd6de6643fefb0fa8da548aa363e291b7d4a37
Kushagra Patel <patelkushagra827@gmail.com>
2026-07-25 20:41:37 +0530

    Updated BNY question

---
+++ Online Assessment/BNY/Q2.cpp
@@ -0,0 +1,17 @@
+/*
+There is n Software module where difficulty of the ith 
+Module you must divide in 3 group Get |d1 - d2| + |d2 - d3|
+
+maximise the sum.
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int helper(){
+    
+}
+int main(){
+
+}
