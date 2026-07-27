85e19d268ffbb2bbc7c1b114140da750871959f6
Kushagra Patel <patelkushagra827@gmail.com>
2026-07-27 22:59:12 +0530

    Solved Missing positive

---
+++ LeetCode/Random Problem/41.cpp
@@ -0,0 +1,31 @@
+/*
+Problem Link - https://leetcode.com/problems/first-missing-positive/
+
+Time - O(n^3);
+Space - O(n^2);
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int>mpp;
+
+        long long maxi = 1;
+
+        for(auto it : nums){
+            maxi = max({it*1LL, maxi});
+            mpp[it]++;
+        }
+        
+        for(long long i = 1; i <= 1LL*(maxi + 1); i++){
+            if(mpp.find(i) == mpp.end()){
+                return i;
+            }
+        } 
+        return -1;
+    }
+};
