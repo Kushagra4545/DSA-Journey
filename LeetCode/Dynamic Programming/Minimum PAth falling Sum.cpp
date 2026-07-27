9674a82164da70e9511492c5de7687f21f1e0d1a
Kushagra Patel <patelkushagra827@gmail.com>
2026-07-27 21:09:54 +0530

    Updated MINIMUM path Falling sum

--- LeetCode/Dynamic Programming/Minimum PAth falling Sum.cpp
+++ LeetCode/Dynamic Programming/Minimum PAth falling Sum.cpp
@@ -12,6 +12,10 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
+        if(row == n-1){
+            return matrix[row][col];
+        }
+
         if(row >= n){
             return 0;
         }
@@ -31,16 +35,54 @@ public:
 
         return dp[row][col] = min({case1, case2, case3});
     }
+
+    int helperTab(vector<vector<int>>& matrix){
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<vector<int>>dp(n, vector<int>(m, 0));
+        
+        for(int i = 0; i < m; i++){
+            dp[n-1][i] = matrix[n-1][i];
+        }
+
+        for(int i = n-2; i >= 0; i--){
+            for(int j = 0; j < m; j++){
+                int case1 = INT_MAX;
+                if(j > 0){
+                    case1 = matrix[i][j] + dp[i+1][j-1];
+                }
+
+                int case2 = matrix[i][j] + dp[i + 1][j];
+
+                int case3 = INT_MAX;
+                if(j < m-1){
+                    case3 = matrix[i][j] + dp[i+1][j+1];
+                }
+
+                dp[i][j] = min({case1, case2, case3});
+            }
+        }
+
+        int mini = INT_MAX;
+
+        for(int i = 0; i < m; i++){
+            mini = min(mini, dp[0][i]);
+        }
+        return mini;
+    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
         vector<vector<int>>dp(n, vector<int>(m, INT_MIN));
 
-        int maxi = INT_MAX;
-        for(int i = 0; i < m; i++){
-            int cost = helper(0, i, matrix, dp);
-            maxi = min(maxi, cost);
-        }
-        return maxi;
+        // int maxi = INT_MAX;
+        // for(int i = 0; i < m; i++){
+        //     int cost = helper(0, i, matrix, dp);
+        //     maxi = min(maxi, cost);
+        // }
+        // return maxi;
+
+        return helperTab(matrix);
     }
 };
