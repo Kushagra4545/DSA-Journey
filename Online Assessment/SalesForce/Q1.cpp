ae2f51f185105577f147311d45408ab9a9a81e0d
Kushagra Patel <patelkushagra827@gmail.com>
2026-07-30 14:04:05 +0530

    Added OA questions

---
+++ Online Assessment/SalesForce/Q1.cpp
@@ -0,0 +1,11 @@
+/*
+Given a array nums with packet and their value represent 
+capacity and a bag with capacity 
+give maximum cost packets you can store
+
+constraints :
+
+1 <= n <= 40
+1 <= bagCapacity <= 1e9
+
+*/
