74e6cb1c37e5de28cb096c2afa2a63402dc1f268
Kushagra Patel <patelkushagra827@gmail.com>
2026-08-01 13:29:29 +0530

    Array Description

---
+++ CSES/Dynamic Programming/array_description.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+int helper(int idx, vector<int>& nums){
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int m;
+    cin>>m;
+
+    vector<int>nums(n);
+    for(int i = 0; i < n; i++){
+        cin>>nums[i];
+    }
+
+}
