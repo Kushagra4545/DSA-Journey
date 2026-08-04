174da17b2b15f7b7f47b29ce0bd146a5f3bf495f
Kushagra Patel <patelkushagra827@gmail.com>
2026-08-04 14:05:59 +0530

    Removed QR code

--- QR code Generator/main.py
+++
@@ -1,8 +0,0 @@
-import qrcode 
-data = "https://leetcode.com/problems/count-of-unfinished-tasks-after-each-shift/description/"
-
-qr = qrcode.make(data)
-
-qr.save("qrcode.png")
-
-print("QR Code created Successfully")
