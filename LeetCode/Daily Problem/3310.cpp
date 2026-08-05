87e7a3b75a526ad6669a1186a7f002c85e3a664a
Kushagra Patel <patelkushagra827@gmail.com>
2026-08-05 22:55:11 +0530

    Solved Daily Problem

---
+++ LeetCode/Daily Problem/3310.cpp
@@ -0,0 +1,79 @@
+/*
+Problem Link - https://leetcode.com/problems/remove-methods-from-project/description/
+
+Time - O((V + E)logV)
+Space - O(V + E)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>>& adjList, vector<int>& visited, vector<int>& sus){
+        visited[node] = 1;
+        sus.push_back(node);
+
+        for(auto it : adjList[node]){
+            if(!visited[it]){
+                dfs(it, adjList, visited, sus);
+            }
+        }
+    }
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>>adjList(n);
+
+        for(auto it : invocations){
+            int u = it[0];
+            int v = it[1];
+
+            adjList[u].push_back(v);
+        }
+
+        vector<int>sus;
+
+        vector<int>visited(n, 0);
+        dfs(k, adjList, visited, sus);
+
+        if(sus.size() == n){
+            return {};
+        }
+
+        unordered_map<int, int>susMpp;
+        for(auto it : sus){
+            susMpp[it]++;
+        }
+
+        bool isReachable = false;
+
+        for(int i = 0; i < adjList.size(); i++){
+            if(susMpp.find(i) != susMpp.end()){
+                continue;
+            }
+
+            for(int j = 0; j < adjList[i].size(); j++){
+                int node = adjList[i][j];
+
+                if(susMpp.find(node) != susMpp.end()){
+                    isReachable = true;
+                    break;
+                }
+            }
+        }
+        vector<int>ans;
+
+        if(isReachable == true){
+            for(int i = 0; i < n; i++){
+                ans.push_back(i);
+            }
+        }
+        else{
+            for(int i =0 ; i < n; i++){
+                if(susMpp.find(i) != susMpp.end()){
+                    continue;
+                }
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
