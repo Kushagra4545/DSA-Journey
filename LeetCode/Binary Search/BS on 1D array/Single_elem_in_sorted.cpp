63ca458a928b21b518014a3c81bf5df466f792a5
Kushagra Patel <patelkushagra827@gmail.com>
2026-08-08 00:02:36 +0530

    Add solution for finding the single element in a sorted array

---
+++ LeetCode/Binary Search/BS on 1D array/Single_elem_in_sorted.cpp
@@ -0,0 +1,46 @@
+/*
+Problem Link = https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1){
+            return nums[0];
+        }
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        int low = 0;
+        int high = n-1;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]){
+                return nums[mid];
+            }
+            if((mid) % 2 == 0){
+                if(nums[mid - 1] == nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else {
+                if(nums[mid - 1] == nums[mid]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
