c3caa7073b57ca70455579e5322bdbc56832f168
Kushagra Patel <patelkushagra827@gmail.com>
2026-08-08 14:13:48 +0530

    Refactor singleNonDuplicate method for improved readability

--- LeetCode/Binary Search/BS on 1D array/Single_elem_in_sorted.cpp
+++ LeetCode/Binary Search/BS on 1D array/Single_elem_in_sorted.cpp
@@ -11,11 +11,10 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1){
-            return nums[0];
-        }
+        if(n == 1) return nums[0];
         if(nums[0] != nums[1]) return nums[0];
         if(nums[n-1] != nums[n-2]) return nums[n-1];
+
         int low = 0;
         int high = n-1;
 
