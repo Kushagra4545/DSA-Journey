bb5ac9f5df55da6239bfd01501839076dc5d4482
Kushagra Patel <patelkushagra827@gmail.com>
2026-08-08 14:09:46 +0530

    Fix formatting of condition in singleNonDuplicate method for consistency

--- LeetCode/Binary Search/BS on 1D array/Single_elem_in_sorted.cpp
+++ LeetCode/Binary Search/BS on 1D array/Single_elem_in_sorted.cpp
@@ -24,7 +24,7 @@ public:
             if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]){
                 return nums[mid];
             }
-            if((mid) % 2 == 0){
+            if(mid % 2 == 0){
                 if(nums[mid - 1] == nums[mid]){
                     high = mid - 1;
                 }
