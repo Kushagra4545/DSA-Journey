195023928a45af86b3bd86285a8f91774efd2704
Kushagra Patel <patelkushagra827@gmail.com>
2026-08-15 13:03:52 +0530

    Refactor labyrinth pathfinding algorithm for clarity and efficiency

--- CSES/Graph/Labyrinth.cpp
+++ CSES/Graph/Labyrinth.cpp
@@ -13,37 +13,44 @@ int main(){
         cin>>grid[i];
     }
 
-    queue<pair<char, pair<int, int>>>q;
-
     vector<vector<int>>visited(n, vector<int>(m, 0));
 
     int delRow[4] = {0, +1, 0, -1};
     int delCol[4] = {+1, 0, -1, 0};
-    char direction[4] = {'R', 'D', 'L', 'U'};
+    char dir[4] = {'R', 'D', 'L', 'U'};
 
-    vector<vector<char>>papa(n, vector<char>(m));
+    vector<vector<pair<int, int>>>parent(n, vector<pair<int, int>>(m, {-1, -1}));
+
+    vector<vector<char>>move(n, vector<char>(m));
+
+    int stRow, stCol;
+    int endRow, endCol;
 
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
+        for(int j =0; j < m; j++){
             if(grid[i][j] == 'A'){
-                q.push({'\0', {i, j}});
-                visited[i][j] = 1;
-                papa[i][j] = '\0';
+                stRow = i;
+                stCol = j;
+            }
+            if(grid[i][j] == 'B'){
+                endRow = i;
+                endCol = j;
             }
         }
     }
+
+    queue<pair<int, int>>q;
+    q.push({stRow, stCol});
+    visited[stRow][stCol] = 1;
 
     while(!q.empty()){
         auto it = q.front();
         q.pop();
 
-        int row = it.second.first;
-        int col = it.second.second;
+        int row = it.first;
+        int col = it.second;
 
-        char parent = it.first;
-
-
-        if(grid[row][col] == 'B'){
+        if(row == endRow && col == endCol){
             break;
         }
 
@@ -51,12 +58,37 @@ int main(){
             int newRow = row + delRow[i];
             int newCol = col + delCol[i];
 
-            if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && !visited[newRow][newCol] && grid[newRow][newCol] != '#'){
-                q.push({parent, {newRow, newCol}});
+            if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && grid[newRow][newCol] != '#' && !visited[newRow][newCol]){
                 visited[newRow][newCol] = 1;
+                parent[newRow][newCol] = {row, col};
+                move[newRow][newCol] = dir[i];
+
+                q.push({newRow, newCol});
             }
         }
     }
 
-    
+    if(visited[endRow][endCol] == 0){
+        cout<<"NO"<<endl;
+        return 0;
+    }
+    string path;
+
+    int r = endRow;
+    int c = endCol;
+
+    while(r != stRow || c != stCol){
+        path.push_back(move[r][c]);
+
+        auto it = parent[r][c];
+
+        r = it.first;
+        c = it.second;
+    }
+
+    reverse(path.begin(), path.end());
+
+    cout<<"YES"<<endl;
+    cout<<path.size()<<endl;
+    cout<<path<<endl;
 }
