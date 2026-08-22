a738e2733349134e88deabaca188963442266025
Kushagra Patel <patelkushagra827@gmail.com>
2026-08-22 23:56:14 +0530

    Add implementation for valid parentheses check using stack

---
+++ LeetCode/Stack And Queue/Monotonic Stack/Valid_paranthesis.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i = 0; i < s.size(); i++){
+            if(st.empty()){
+                if(s[i] == ')' || s[i] == ']' || s[i] == '}'){
+                    return false;
+                }
+                st.push(s[i]);
+                continue;
+            }
+            if(s[i] == ')' && st.top() == '('){
+                st.pop();
+            }
+            else if(s[i] == '}' && st.top() == '{'){
+                st.pop();
+            }
+            else if(s[i] == ']' && st.top() == '['){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        if(st.empty()) return true;
+        return false;
+    }
+};
