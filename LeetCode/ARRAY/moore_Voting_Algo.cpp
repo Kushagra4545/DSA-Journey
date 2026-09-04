628700a743726c4a064d9d844cc7b30331832dde
Kushagra Patel <patelkushagra827@gmail.com>
2026-09-04 21:58:54 +0530

    Add implementation of Moore Voting Algorithm to find the majority element in an array

---
+++ LeetCode/ARRAY/moore_Voting_Algo.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*-------MOORE VOTING ALGORITHM--------*/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int element = 0;
+        int count = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(count == 0){
+                element = nums[i];
+            }
+
+            if(nums[i] == element){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return element;
+    }
+};
