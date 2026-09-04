4664e0e7215664b12b61490ab198b18a2992d31c
Kushagra Patel <patelkushagra827@gmail.com>
2026-09-04 21:33:46 +0530

    Add implementation of Dutch National Flag algorithm to sort colors in an array

---
+++ LeetCode/ARRAY/sort colors.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*------DUTCH NATIONAL FLAG ALGORITHM---------*/
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[mid], nums[low]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else if(nums[mid] == 2){
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
