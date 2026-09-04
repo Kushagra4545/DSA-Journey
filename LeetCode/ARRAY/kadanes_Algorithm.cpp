5d878e985c8e1f15623cdf19ba42e4b2c107084d
Kushagra Patel <patelkushagra827@gmail.com>
2026-09-04 23:03:57 +0530

    Add implementation of Kadane's Algorithm to find the maximum subarray sum

---
+++ LeetCode/ARRAY/kadanes_Algorithm.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* ----------- KADANES ALGORITHM---------- */
+
+int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+
+        int maxSum = INT_MIN;
+        int sum = 0;
+
+        for(int i = 0; i < n; i++){
+            if(sum < 0) {
+                sum = 0;
+            }
+            sum += nums[i];
+            maxSum = max(maxSum, sum);
+        }
+        return maxSum;
+    }
